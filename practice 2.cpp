6aa379fad96433f6cca56bd65ccb1c50e9b72817
blacktiger05 <oceanflow372@gmail.com>
2026-02-20 15:25:48 +0500

    Add files via upload

---
+++ practice 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	short x;
+	short y;
+	short z;
+	
+	cout << "please enter a digit" << endl;
+	cin >> x;
+
+	cout << "please enter second digit" << endl;
+	cin >> y;
+	
+	cout << "the sum will be " << "=" << endl;
+	z = x + y;
+	cout << z << endl;
+}
